ea03070b9bcbd9192c7afe9ce4de9b58a49d3d14
koosco <rnxogud123@naver.com>
2024-08-20 01:37:18 +0900

    [style] google code style 적용

--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -10,26 +10,28 @@ import org.fastcampus.student_management.domain.Student;
 import org.fastcampus.student_management.repo.CourseRepository;
 
 public class CourseService {
-  private final CourseRepository courseRepository;
-  private final StudentService studentService;
 
-  public CourseService(CourseRepository courseRepository, StudentService studentService) {
-    this.courseRepository = courseRepository;
-    this.studentService = studentService;
-  }
+    private final CourseRepository courseRepository;
+    private final StudentService studentService;
 
-  public void registerCourse(CourseInfoDto courseInfoDto) {
-    Student student = studentService.getStudent(courseInfoDto.getStudentName());
-    Course course = new Course(student, courseInfoDto.getCourseName(), courseInfoDto.getFee(), courseInfoDto.getDayOfWeek(), courseInfoDto.getCourseTime());
-    courseRepository.save(course);
-  }
+    public CourseService(CourseRepository courseRepository, StudentService studentService) {
+        this.courseRepository = courseRepository;
+        this.studentService = studentService;
+    }
 
-  public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
-    // TODO: 과제 구현 부분
-    return new ArrayList<>();
-  }
+    public void registerCourse(CourseInfoDto courseInfoDto) {
+        Student student = studentService.getStudent(courseInfoDto.getStudentName());
+        Course course = new Course(student, courseInfoDto.getCourseName(), courseInfoDto.getFee(),
+            courseInfoDto.getDayOfWeek(), courseInfoDto.getCourseTime());
+        courseRepository.save(course);
+    }
 
-  public void changeFee(String studentName, int fee) {
-    // TODO: 과제 구현 부분
-  }
+    public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
+        // TODO: 과제 구현 부분
+        return new ArrayList<>();
+    }
+
+    public void changeFee(String studentName, int fee) {
+        // TODO: 과제 구현 부분
+    }
 }
